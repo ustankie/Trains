4d7f5b707e3256c9d6ae70771b75efff9d5d5537
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-04-21 20:39:58 +0200

    Prepared hero files

---
+++ trains_front/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import "../styles/Hero.css";
+
+export default function Hero() {
+    return (
+        <div className="hero--wrapper">
+            <div className="background">
+                <div className="content">
+                    <div className="hero--text">
+                        <p><span className="blue">Travel</span> is the only thing you buy that makes you <span className="blue">richer</span></p>
+                    </div>
+                    <div className="hero--search-box">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
