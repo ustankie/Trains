a7e58377c114ffa7ccb2108c912a1d50d9f34bd6
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-04-28 20:13:32 +0200

    Added stations endpoint to springboot

---
+++ springboot/demo/src/main/java/com/trains/demo/model/Station.java
@@ -0,0 +1,21 @@
+package com.trains.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "all_stations")
+@Getter
+@Setter
+public class Station {
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
