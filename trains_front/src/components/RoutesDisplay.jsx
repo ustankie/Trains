957403e622e04cf2480625b4684cd9881b7040b0
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-04-28 22:33:37 +0200

    Redirected routes to new page

--- trains_front/src/components/RoutesDisplay.jsx
+++ trains_front/src/components/RoutesDisplay.jsx
@@ -1,54 +1,51 @@
 import React from "react";
-import '../styles/RoutesDisplay.css'
-import { Card, Button, Image } from 'react-bootstrap'
-import tom from '../images/sad_tom.png'
+import '../styles/RoutesDisplay.css';
+import '../styles/Login.css'
+import tom from '../images/sad_tom.png';
+import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom"
+import { Card, Button } from 'react-bootstrap';
 
-
-export default function RoutesDisplay({data}) {
-    const len=(data ? data.length : 0);
+export default function RoutesDisplay() {
+    const location = useLocation();
+    const data = location.state?.data;
 
     return (
         <>
-            <div>
-                {data && len>0 ? (
-                    <div className='cardWrapper'>
-                        {data.map((route, index) => (!len || index < len) ? (
-                            <Card key={route.routeId} className= "routeCard"  >
-                                <Card.Body className="routeCardBody">
-                                    <div className="routeCardBodyContent">
-
-                                        <div className="routeDetails">
-                                            <a className="routeCardHeaders">Departure</a>
-                                            <p className="routeCardDetails">{route.departureTime}</p>
-                                        </div>
-                                        <div className="routeDetails">
-                                            <a className="routeCardHeaders" >Arrival</a>    
-                                            <p className="routeCardDetails">{route.arrivalTime}</p>
-                                        </div>
-                                        <div className="routeDetails">
-                                            <a className="routeCardHeaders" >Total Price</a>
-                                            <p className="routeCardDetails">{route.price} PLN</p>
-                                        </div>
-                                        <Button className="blue--btn">Book</Button>
-
+            <div className="home--link"><Link to="/">TRAIN <span className="blue">SERVICE</span></Link></div>
+            {data && data.length > 0 ? (
+                
+                <div className='cardWrapper'>
+                    {data.map((route) => (
+                        <Card key={route.routeId} className="routeCard">
+                            <Card.Body className="routeCardBody">
+                                <div className="routeCardBodyContent">
+                                    <div className="routeDetails">
+                                        <p className="routeCardHeaders">Departure</p>
+                                        <p className="routeCardDetails">{route.departureTime}</p>
                                     </div>
-                                </Card.Body>
-                            </Card>
-                        ):(null))}
+                                    <div className="routeDetails">
+                                        <p className="routeCardHeaders">Arrival</p>
+                                        <p className="routeCardDetails">{route.arrivalTime}</p>
+                                    </div>
+                                    <div className="routeDetails">
+                                        <p className="routeCardHeaders">Total Price</p>
+                                        <p className="routeCardDetails">{route.price} PLN</p>
+                                    </div>
+                                    <Button className="blue--btn">Book</Button>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </div>
+            ) : (
+                <div className="backgroundBottom">
+                    <div className="noRouteFound">
+                        <p id="noRoutesText">No routes found...</p>
+                        <img src={tom} alt="Sad Tom" id="tom"/>
                     </div>
-                    
-                ):data && len === 0 ? (
-                    <div className="backgroundBottom">
-                        <div className="noRouteFound">
-                            <p id="noRoutesText">No routes found...</p>
-                            <img src={tom} alt="Sad Tom" id="tom"/>
-                        </div>
-                    </div>
-                ) : (
-                    null
-                )}
-            </div>
+                </div>
+            )}
         </>
     );
-
 }
