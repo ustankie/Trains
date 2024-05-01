577416c97bad089a6cc19817157944dbb7abb801
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2024-05-01 13:27:43 +0200

    Partially working reservation endpoint

--- springboot/demo/src/main/java/com/trains/demo/controller/ReservationController.java
+++ springboot/demo/src/main/java/com/trains/demo/controller/ReservationController.java
@@ -1,2 +1,32 @@
-package com.trains.demo.controller;public class ReservationController {
+package com.trains.demo.controller;
+
+
+import com.trains.demo.model.Reservation;
+import com.trains.demo.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reservations")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @PostMapping("/add")
+    public void addReservation(@RequestBody Reservation request) {
+         reservationService.addReservation(
+                request.getUserId(),
+                request.getDiscountId(),
+                request.getRouteId(),
+                request.getStartStationId(),
+                request.getEndStationId(),
+                request.getDepartureDate(),
+                request.getSeatId()
+        );
+
+    }
 }
