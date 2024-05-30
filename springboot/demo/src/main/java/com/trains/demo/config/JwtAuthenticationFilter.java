b889e2c92529f551bce7c9c0ba431b5c0c5a8246
ustankie <ustankiewicz@student.agh.edu.pl>
2024-05-30 12:05:51 +0200

    Change the way of retrieving user and add navbar

--- springboot/demo/src/main/java/com/trains/demo/config/JwtAuthenticationFilter.java
+++ springboot/demo/src/main/java/com/trains/demo/config/JwtAuthenticationFilter.java
@@ -1,6 +1,6 @@
 package com.trains.demo.config;
 
-import com.trains.demo.services.AuthenticationService;
+import com.trains.demo.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,7 +20,7 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final AuthenticationService.JwtService jwtService;
+    private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
     @Override
---
+++ springboot/demo/src/main/java/com/trains/demo/services/JwtService.java
@@ -0,0 +1,70 @@
+package com.trains.demo.services;
+
+import com.trains.demo.controller.AuthController;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    @Value("${security.key}")
+    private String SECRET_KEY;
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+    public String extractUserLogin(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(getSignInKey()).build().parseSignedClaims(token).getPayload();
+
+
+    }
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+1000*60*20))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        String username=extractUserLogin(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims=extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+}
