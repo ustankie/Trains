a795deb9479322b6faee7aa5ec02f1712c5fb388
ustankie <ustankiewicz@student.agh.edu.pl>
2024-06-09 18:38:43 +0200

    Fix appearing user_history table in db and minor bugs

--- springboot/demo/src/main/java/com/trains/demo/model/ReservationHistory.java
+++ springboot/demo/src/main/java/com/trains/demo/model/ReservationHistory.java
@@ -1,45 +1,25 @@
 package com.trains.demo.model;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import jakarta.persistence.Id;
-import org.springframework.data.annotation.Transient;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Date;
 
-@Entity
-@Data
-public class ReservationHistory {
-    @Id
-    private Long reservationId;
-    private Long routeId;
-    private LocalTime departure;
-    private LocalTime arrival;
-    private String startStation;
-    private String endStation;
-    private Long seatId;
-    private LocalDate departureDate;
-    private Double price;
+public interface ReservationHistory {
 
+    Long getReservation_id();
 
-    public ReservationHistory(Long reservationId, Long routeId, LocalTime departure, LocalTime arrival, String startStation, String endStation, Long seatId, LocalDate departureDate, Double price) {
-        this.reservationId = reservationId;
-        this.routeId = routeId;
-        this.departure = departure;
-        this.arrival = arrival;
-        this.startStation = startStation;
-        this.endStation = endStation;
-        this.seatId = seatId;
-        this.departureDate = departureDate;
-        this.price = price;
-    }
+    Long getRoute_id();
 
-    public ReservationHistory() {
+    LocalTime getDeparture();
 
-    }
+    LocalTime getArrival();
 
+    String getStartStation();
+
+    String getEndStation();
+
+    Long getSeatId();
+
+    LocalDate getDepartureDate();
+
+    Double getPrice();
 }
